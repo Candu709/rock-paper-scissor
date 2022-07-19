3e522ac9c578f05508d09303c69e9338eb8ac9a9
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-07-19 15:42:55 +0200

    Add getRandomInt and getComputerChoice functions

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,24 @@ const shapes = [
   `✌ Scissors`
 ];
 // Ask the player for an input
-  // Check the input against the list:
-    // If invalid ask again
+// Check the input against the list:
+// If invalid ask again
+
 // Pick a random shape from the list (Computer "choice")
+function getComputerChoice(){
+  return shapes[Math.floor(Math.random()*shapes.length)]
+}
+for (let i = 0; i < 10; i++) {
+  console.log(getComputerChoice());
+
+}
+let a = shapes.length
+console.log(`-> ${a}`)
 // Compare the player choice and the machine choice to determine winner
-// Add point to the winner
+//  the player picks first to prevent cheating using Dev tools
+  // Add point to the winner
 
 const rounds = 5;
 // Game: Repeat round until one of the players points is > 1/2 of rounds
+
   // Declare winner
