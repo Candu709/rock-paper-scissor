a85d0533e9b48c3a4611c2476987842be7100f5a
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-07-19 02:12:59 +0200

    Add script.js

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// Store a list of the possible hand shapes
+//   (should prob be an of object but havent dealt with those yet)
+const shapes = [
+  `✊ Rock`,
+  `✋ Paper`,
+  `✌ Scissors`
+];
+// Ask the player for an input
+  // Check the input against the list:
+    // If invalid ask again
+// Pick a random shape from the list (Computer "choice")
+// Compare the player choice and the machine choice to determine winner
+// Add point to the winner
+
+const rounds = 5;
+// Game: Repeat round until one of the players points is > 1/2 of rounds
+  // Declare winner
