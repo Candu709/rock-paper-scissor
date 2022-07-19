93dfd89ec5a1e20ff446c032df978dee6e9d274a
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-07-19 17:03:48 +0200

    Add getPlayerChoice and validateInput functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,57 @@
 // Store a list of the possible hand shapes
-//   (should prob be an of object but havent dealt with those yet)
+//   (should prob be an object but havent dealt with those yet)
 const shapes = [
   `✊ Rock`,
   `✋ Paper`,
   `✌ Scissors`
 ];
+
 // Ask the player for an input
+function getPlayerChoice(){
+  let playerChoice
+  do {
+    playerChoice = prompt(`Please type in the name of a hand shape
+    ✊ Rock
+    ✋ Paper
+    ✌ Scissors
+    `)
+    playerChoice = validateInput(playerChoice)
+    // If invalid ask again
+  } while (!playerChoice)
+  return playerChoice
+}
+
 // Check the input against the list:
-// If invalid ask again
+function validateInput(input){
+  input = input.trim()
+  shapeName = input.toLowerCase()
+  switch (shapeName) {
+    case `rock`:
+    return shapes[0];
+    case `paper`:
+    return shapes[1];
+    case `scissors`:
+    return shapes[2];
+    default:
+    alert(`${input} is not a valid shape`)
+    return false;
+  }
+}
 
 // Pick a random shape from the list (Computer "choice")
-function getComputerChoice(){
-  return shapes[Math.floor(Math.random()*shapes.length)]
+function getRandomInt(min,maxExluded){
+  return Math.floor(Math.random() * (maxExluded - min) + min)
 }
-for (let i = 0; i < 10; i++) {
-  console.log(getComputerChoice());
 
+function getComputerChoice(){
+  return shapes[getRandomInt(0,shapes.length)]
 }
-let a = shapes.length
-console.log(`-> ${a}`)
-// Compare the player choice and the machine choice to determine winner
+
+// Compare the player's and the machine's choice to determine winner
 //  the player picks first to prevent cheating using Dev tools
-  // Add point to the winner
+// Add point to the winner
 
 const rounds = 5;
 // Game: Repeat round until one of the players points is > 1/2 of rounds
 
-  // Declare winner
+// Declare winner
