32ae2360a7b72db885b9dcd3b3c105dba40a1121
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-07-21 01:48:22 +0200

    Fix iterator variable missuse

--- scripts/main.js
+++ scripts/main.js
@@ -79,10 +79,10 @@ const rounds = new Array(5)
 function playGame(){
   let playerScore = 0
   let computerScore = 0
-  for (const i of rounds) {
-    rounds[i] = playRound(getPlayerChoice(), shapes[0])
-    if (rounds[i]) ++playerScore;
-    if (rounds[i] == false) ++computerScore;
+  for (const round of rounds) {
+    round = playRound(getPlayerChoice(), shapes[0])
+    if (round) ++playerScore;
+    if (round == false) ++computerScore;
     if (playerScore > (rounds.length/2)) {
       console.log(`You Win!`);
       return `You Win!`;
