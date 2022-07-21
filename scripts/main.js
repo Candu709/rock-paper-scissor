e34a344093366e3e4833c8d863d82ec3f7963177
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-07-21 18:17:52 +0200

    Move playRound into playGame to simplify scores
    By having the playRound function nested inside the playGame function
    it is possible to incremente both playerScore and computerScore
    directly into the playRound comparison without having to revaluate the
    results of it later

--- scripts/main.js
+++ scripts/main.js
@@ -47,44 +47,15 @@ function getComputerChoice(){
   return shapes[getRandomInt(0,shapes.length)]
 }
 
-// Compare the player's and the machine's choice to determine winner
-//  the player picks first to prevent cheating using Dev tools
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection == computerSelection) {
-    let message = `It's a Tie! You both played ${playerSelection}\nTry again`;
-    console.log(message);
-    alert(message);
-    return playRound(getPlayerChoice(),getComputerChoice());
-  };
-  switch (true) {
-    default:
-      message = `You lose! ${computerSelection} Beats ${playerSelection}`;
-      console.log(message);
-      alert(message);
-      return false;
-      // Matchups that the player wins
-    case playerSelection == shapes[0] && computerSelection == shapes[2]:
-    case playerSelection == shapes[1] && computerSelection == shapes[0]:
-    case playerSelection == shapes[2] && computerSelection == shapes[1]:
-      message = `You win! ${playerSelection} Beats ${computerSelection}`;
-      console.log(message);
-      alert(message);
-      return true;
-  }
-}
-
 // Game: Repeat round until one of the players points is > 1/2 of rounds
 function playGame(){
   // rounds length has to be odd to ensure a zero-sum game
   const rounds = new Array(5).fill(' ')
-function playGame(){
   let playerScore = 0
   let computerScore = 0
   for (i = 0; i < rounds.length; i++) {
     // computer fixed to stone for testing purposes, tie for random
     rounds[i] = playRound(getPlayerChoice(), shapes[0])
-    if (rounds[i]) ++playerScore;
-    if (rounds[i] == false) ++computerScore;
     if (playerScore > (rounds.length/2)) {
       console.log(`You Win!`);
       return `You Win!`;
@@ -93,6 +64,33 @@ function playGame(){
       console.log(`You lose.`);
       return `You lose.`;
     };
+  }
+
+  // Compare the player's and the machine's choice to determine winner
+  function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+      let message = `It's a Tie! You both played ${playerSelection}\nTry again`;
+      console.log(message);
+      alert(message);
+      return playRound(getPlayerChoice(),getComputerChoice());
+    };
+    switch (true) {
+      default:
+        ++computerScore
+        message = `You lose! ${computerSelection} Beats ${playerSelection}`;
+        console.log(message);
+        alert(message);
+        return false;
+      // Matchups that the player wins
+      case playerSelection == shapes[0] && computerSelection == shapes[2]:
+      case playerSelection == shapes[1] && computerSelection == shapes[0]:
+      case playerSelection == shapes[2] && computerSelection == shapes[1]:
+        ++playerScore
+        message = `You win! ${playerSelection} Beats ${computerSelection}`;
+        console.log(message);
+        alert(message);
+        return true;
+    }
   }
 }
 // Declare winner
