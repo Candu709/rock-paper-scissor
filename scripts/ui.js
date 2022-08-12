1aed69f2b92f887f11dec1a0e014713497c9bc50
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-08-12 18:20:12 +0200

    Add buttons for each shape

---
+++ scripts/ui.js
@@ -0,0 +1,10 @@
+const body = document.querySelector('body');
+
+function makeButton(shape) {
+  const button = document.createElement('button');
+  button.textContent = shape;
+  button.addEventListener('click', playRound(shape, getComputerChoice))
+  body.append(button);
+}
+
+shapes.forEach(shape => makeButton(shape));
