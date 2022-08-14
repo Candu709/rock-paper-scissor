9b486204731d891d9ae321aedf8db25d4e215d53
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-08-14 20:13:39 +0200

    Fix onclick events not calling function correctly

--- scripts/ui.js
+++ scripts/ui.js
@@ -3,7 +3,7 @@ const body = document.querySelector('body');
 function makeButton(shape) {
   const button = document.createElement('button');
   button.textContent = shape;
-  button.addEventListener('click', playRound(shape, getComputerChoice))
+  button.addEventListener('click', () => playRound(shape, getComputerChoice()))
   body.append(button);
 }
 
