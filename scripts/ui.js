e7faf4516b8333e192416ae2c89428a15f737d6b
Emanuele Cantore <56078808+Candu709@users.noreply.github.com>
2022-08-22 20:17:20 +0200

    Refactor makeButton to untie it from shapes

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,10 +1,18 @@
 const body = document.querySelector('body');
 
-function makeButton(shape) {
+function makeButton(text, id = '', theParent = 'body', ...classes) {
   const button = document.createElement('button');
-  button.textContent = shape;
-  button.addEventListener('click', () => playRound(shape, getComputerChoice()))
-  body.append(button);
+  if (id) button.id = id;
+  if (text) button.textContent = text;
+  if (classes.length !== 0) classes.forEach((eachClass) => button.classList.add(eachClass));
+  const parent = document.querySelector(theParent);
+  parent.appendChild(button);
 }
 
-shapes.forEach(shape => makeButton(shape));
+shapes.forEach(shape => makeButton(shape, shape, '#shapes', 'shape'));
+const shapeButtons = document.querySelectorAll('button.shape');
+shapeButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    playRound(button.id, getComputerChoice());
+  });
+});
